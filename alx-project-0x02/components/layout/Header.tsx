883947995f8130ade5363f7b1f1f3959b72102fc
ben-m-m <mainaben23@gmail.com>
2025-12-13 21:03:37 +0300

    feat: update Header component navigation links and remove Contact link

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -11,14 +11,11 @@ const Header = () => {
             </Link>
           </div>
           <nav className="flex space-x-4">
-            <Link href="/" className="text-gray-700 hover:text-gray-900">
+            <Link href="/home" className="text-gray-700 hover:text-gray-900">
               Home
             </Link>
             <Link href="/about" className="text-gray-700 hover:text-gray-900">
               About
-            </Link>
-            <Link href="/contact" className="text-gray-700 hover:text-gray-900">
-              Contact
             </Link>
           </nav>
         </div>

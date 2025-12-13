8536e90e098e5cc1367baffe08926c9ad41679a5
ben-m-m <mainaben23@gmail.com>
2025-12-13 22:35:37 +0300

    fix: update import statements for Header component to use absolute paths

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import Header from "../components/layout/Header";
+import Header from "@/components/layout/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

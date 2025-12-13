ca0bfd1491830ca4944742e71d2cbf683dd88de9
ben-m-m <mainaben23@gmail.com>
2025-12-13 23:11:17 +0300

    8

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,31 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
-const Users: React.FC = () => {
-  const [users, setUsers] = useState<UserProps[]>([]);
+interface UsersPageProps {
+  users: UserProps[];
+}
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
-      const data = await response.json();
-
-      const formattedUsers: UserProps[] = data.map((user: any) => ({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        address: {
-          street: user.address.street,
-          city: user.address.city,
-        },
-      }));
-
-      setUsers(formattedUsers);
-    };
-
-    fetchUsers();
-  }, []);
-
+const Users: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-4">Users</h1>
@@ -38,5 +19,26 @@ const Users: React.FC = () => {
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  const users: UserProps[] = data.map((user: any) => ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    address: {
+      street: user.address.street,
+      city: user.address.city,
+    },
+  }));
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
 
 export default Users;
